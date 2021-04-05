060354adf72bc930816b4496b7e6a933bf753745
Jacob Loris <jacobloris@outlook.com>
2021-04-05 00:46:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,10 @@ def display_board(board)
 end
 
 def position_taken?(board, index)
-  
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
